7ca6638f18c55a505c0c7a229fe33c76ae0cec56
aynuryalcin <aynurylcn@gmail.com>
2014-08-23 01:39:08 +0300

    fibonacci
    Proje .net C# konsol ekranında yazılmış ve fibonacci sayılarını gösteren
    progrramdır.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç elemanı gösterilsin:  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[a];
+            b[0] = 1;
+            b[1] = 1;
+            Console.Write(b[0] + "\t");
+            Console.Write(b[1] + "\t");
+            for (int i = 2; i <a; i++)
+            {
+                b[i] = b[i - 1] + b[i - 2];
+               Console.Write(b[i]+"\t");
+               
+            } 
+            Console.ReadKey();
+        }
+    }
+}
